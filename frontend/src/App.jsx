a3bbf3d2a193428ee81476895dff2ffc7410529b
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-06 20:22:26 +0100

    refactor: comment out unused state and effect hooks in App componen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios';
-import './App.css'
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('')
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    axios.get('/api')
-      .then(response => setMessage(response.data.message))
-      .catch(error => console.error('Error:', error))
-  }, [])
+  // useEffect(() => {
+  //   axios
+  //     .get("/api")
+  //     .then((response) => setMessage(response.data.message))
+  //     .catch((error) => console.error("Error:", error));
+  // }, []);
 
   return (
     <div>
-<h1 class="text-3xl m-2 font-bold underline">
-    Hello world!
-  </h1>
-        <p>Backend says: {message}</p>
+      <h1 className="text-3xl m-2 font-bold underline">Hello world!</h1>
+      <p>Backend says: Hello</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

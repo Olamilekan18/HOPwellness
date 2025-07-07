a37e17400597c2e023b5aa8a720586c1c2893517
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-07 11:59:06 +0100

    fix: uncomment and update API call in App component

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,20 +1,22 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  // const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-  // useEffect(() => {
-  //   axios
-  //     .get("/api")
-  //     .then((response) => setMessage(response.data.message))
-  //     .catch((error) => console.error("Error:", error));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
     <div>
       <h1 className="text-3xl m-2 font-bold underline">Hello world!</h1>
-      <p>Backend says: Hello</p>
+      <p>Backend says: {message}</p>
     </div>
   );
 }

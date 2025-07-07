ed0a273dbe4a2e792ebfadcffaa2ea594a4032ab
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-07 12:38:37 +0100

    refactor: simplify App component by removing unused state and API call

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,24 +1,5 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/")
-      .then((response) => {
-        setMessage(response.data);
-      })
-      .catch((error) => console.error("Error:", error));
-  }, []);
-
-  return (
-    <div>
-      <h1 className="text-3xl m-2 font-bold underline">Hello world!</h1>
-      <p>Backend says: {message}</p>
-    </div>
-  );
+  return <h1>Hello</h1>;
 }
 
 export default App;

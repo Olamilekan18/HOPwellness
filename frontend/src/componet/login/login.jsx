5dc7a5fd9989e4c0db4def4df33bc6aad56655b3
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-08 09:00:19 +0100

    feat: remove unnecessary comment from LoginForm component

--- frontend/src/componet/login/login.jsx
+++ frontend/src/componet/login/login.jsx
@@ -46,7 +46,7 @@ export default function LoginForm() {
                   type="password"
                   placeholder="Password"
                 />
-                {/* Animate Login Button */}
+
                 <motion.button
                   className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                   whileHover={{ scale: 1.05 }}

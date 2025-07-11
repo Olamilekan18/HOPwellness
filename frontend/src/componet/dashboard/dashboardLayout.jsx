86aabdbf39ff5aeb1de7bbc2dfa375b133b62fde
Areo Ayomide <areoayomide2008@gmail.com>
2025-07-11 08:02:10 +0100

    changing the dark mode and light mode and getting them from localstorage

--- frontend/src/componet/dashboard/dashboardLayout.jsx
+++ frontend/src/componet/dashboard/dashboardLayout.jsx
@@ -20,7 +20,7 @@ import defaultImage from "/blank-profile-picture-973460_960_720.webp";
 import { useEffect } from "react";
 export default function DashboardLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState();
   const [profilePicture, setProfilePicture] = useState("");
   const menuItems = [
     {
@@ -93,7 +93,20 @@ export default function DashboardLayout({ children }) {
     } else {
       setProfilePicture(savedProfilePicture);
     }
+    const darkMode = localStorage.getItem("darkMode");
+
+    if (darkMode === null) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(darkMode === "true");
+    }
   }, []);
+
+  useEffect(() => {
+    if (darkMode !== undefined) {
+      localStorage.setItem("darkMode", darkMode);
+    }
+  }, [darkMode]);
 
   return (
     <div

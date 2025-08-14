8c802bd55b69d5ca47a2c5017c9102ef365c86f3
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-08-14 23:07:03 +0100

    refactor: wrap EachCommunityPage content in Layout component for improved structure

--- frontend/src/pages/dashboard/eachCommunityPage.jsx
+++ frontend/src/pages/dashboard/eachCommunityPage.jsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
+import Layout from "./components/Layout";
+import RightRail from "./components/RightRail";
+
 export default function EachCommunityPage() {
   const { communityId } = useParams();
   console.log(communityId);
 
   return (
-    <div>
-      <h1>Community Details</h1>
-      <p>Community ID: {communityId}</p>
-    </div>
+    <Layout current="communities" right={<RightRail />}>
+      <div>
+        <h1>Community Details</h1>
+        <p>Community ID: {communityId}</p>
+      </div>
+    </Layout>
   );
 }
